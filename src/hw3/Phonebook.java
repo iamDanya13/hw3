eaf6257286997530f3eb00c2515c37cc0f4a34c9
iamDanya13 <67544472+iamDanya13@users.noreply.github.com>
2020-10-29 00:14:36 +0300

    hw3

---
+++ src/hw3/Phonebook.java
@@ -0,0 +1,18 @@
+package hw3;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Phonebook {
+
+    private HashMap<String, List<String>> book;
+
+    public Phonebook() {
+        this.book = new HashMap<>();
+    }
+
+    public void add(String surname, String number) {
+
+    }
+}
